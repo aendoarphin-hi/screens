dc9adc5c706c97793ce90cf79532302b6249c6d3
Arhon <aronp2000@gmail.com>
2026-09-19 23:42:42 -0500

    remove form markup, reformat

--- src/components/modals/UploadContentModalComponent.vue
+++ src/components/modals/UploadContentModalComponent.vue
@@ -1,47 +1,47 @@
 <template>
-  <form @submit.prevent="uploadContent">
-    <div class="modal fade" id="upload-content-modal" ref="uploadContentModal" tabindex="-1">
-      <div class="modal-dialog modal-dialog-centered" style="max-width: 500px;">
-        <div class="modal-content shadow">
-          <div class="modal-header">
-            <div class="d-flex align-items-center w-100">
-              <strong class="text-nowrap overflow-hidden me-4" style="text-overflow: ellipsis">
-                Submit Content
-              </strong>
+  <div class="modal fade" id="upload-content-modal" ref="uploadContentModal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered" style="max-width: 500px;">
+      <div class="modal-content shadow">
+        <div class="modal-header">
+          <div class="d-flex align-items-center w-100">
+            <strong class="text-nowrap overflow-hidden me-4" style="text-overflow: ellipsis">
+              Submit Content
+            </strong>
+          </div>
+        </div>
+        <div class="modal-body">
+          <!-- help description -->
+          <small>Submit (jpg, jpeg, png, pdf) files so they can be added to a playlist and assigned to
+            screens.</small>
+          <span style="font-size: 8pt; line-height: 5px;" class="text-muted fst-italic">NOTE: If submitted by a user in
+            the 'Supervisors' group, HR will need to approve the files before it becomes available</span>
+          <transition enter-active-class="animate__animated animate__fadeIn animate__faster">
+            <!-- error message -->
+            <div v-if="error" class="mb-2 p-2 small rounded bg-danger-subtle text-danger-emphasis">
+              {{ error }}
             </div>
-          </div>
-          <div class="modal-body">
-            <!-- help description -->
-            <small>Submit (jpg, jpeg, png, pdf) files so they can be added to a playlist and assigned to
-              screens.</small>
-            <span style="font-size: 8pt; line-height: 5px;" class="text-muted fst-italic">NOTE: If submitted by a user in the 'Supervisors' group, HR will need to approve the files before it becomes available</span>
-            <transition enter-active-class="animate__animated animate__fadeIn animate__faster">
-              <!-- error message -->
-              <div v-if="error" class="mb-2 p-2 small rounded bg-danger-subtle text-danger-emphasis">
-                {{ error }}
-              </div>
-            </transition>
+          </transition>
 
-            <!-- file upload -->
-            <!-- TODO: add a drag and drop area to upload files -->
-            <div class="d-flex flex-wrap mb-2 gap-1">
-              <span v-for="s in submittedFiles" :key="s" class="badge rounded-pill bg-primary-subtle text-primary-emphasis me-1">{{ s }}</span>
-            </div>
+          <!-- file upload -->
+          <!-- TODO: add a drag and drop area to upload files -->
+          <div class="d-flex flex-wrap mb-2 gap-1">
+            <span v-for="s in submittedFiles" :key="s"
+              class="badge rounded-pill bg-primary-subtle text-primary-emphasis me-1">{{ s }}</span>
           </div>
-          <div class="modal-footer p-2">
-            <button type="reset" class="btn btn-sm btn-danger me-2" data-bs-dismiss="modal" @click="clearChanges"
-              title="Cancel">
-              Cancel
-            </button>
+        </div>
+        <div class="modal-footer p-2">
+          <button type="reset" class="btn btn-sm btn-danger me-2" data-bs-dismiss="modal"
+            title="Cancel">
+            Cancel
+          </button>
 
-            <button type="submit" class="btn btn-sm btn-success" title="Upload Content">
-              + Upload
-            </button>
-          </div>
+          <button @click="upload" type="submit" class="btn btn-sm btn-success" title="Upload Content">
+            + Upload
+          </button>
         </div>
       </div>
     </div>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -66,9 +66,7 @@ export default {
   },
   methods: {
     upload() {
-      // console log the files the user has uploaded
-      const files = document.getElementById('upload-content-file').files;
-      console.log(files);
+      window.alert('todo: upload files');
     },
   }
 }
