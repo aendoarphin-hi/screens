8f7608c62add203009e9e9b2883443769c1c9d60
Arhon <aronp2000@gmail.com>
2026-09-20 23:10:37 -0500

    add path for build

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: '/ttprod/v3/hrcomms/app/',
   devServer: {
     port: 8080,
     proxy: {
